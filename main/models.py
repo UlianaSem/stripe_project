932c9f35a152f51cb1eab67466f1fe872f9411f3
Uliana Semyonova <i@ulianasemyonova.ru>
2023-12-21 18:41:26 +0500

    Create Item model in main app

--- main/models.py
+++ main/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Item(models.Model):
+    name = models.CharField(max_length=255, verbose_name='название')
+    description = models.TextField(verbose_name='описание', null=True, blank=True)
+    price = models.FloatField(verbose_name='цена')
+
+    def __str__(self):
+        return f'{self.name}, цена {self.price}'
+
+    class Meta:
+        verbose_name = 'товар'
+        verbose_name_plural = 'товары'
