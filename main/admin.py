2e7e722f7f83f67807368c5e6d97bb471024e729
Uliana Semyonova <i@ulianasemyonova.ru>
2024-01-03 18:04:54 +0500

    Create Discount, Tax, ItemOrder, Order models

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 
-from main.models import Item
+from main.models import Item, Discount, Tax, ItemOrder, Order
 
 admin.site.register(Item)
+
+admin.site.register(Discount)
+
+admin.site.register(Tax)
+
+admin.site.register(ItemOrder)
+
+admin.site.register(Order)
--- main/models.py
+++ main/models.py
@@ -12,3 +12,54 @@ class Item(models.Model):
     class Meta:
         verbose_name = 'товар'
         verbose_name_plural = 'товары'
+
+
+class Discount(models.Model):
+    """Модель Discount, которую можно прикрепить к модели Order"""
+    value = models.FloatField(verbose_name='размер скидки')
+
+    def __str__(self):
+        return self.value
+
+    class Meta:
+        verbose_name = 'скидка'
+        verbose_name_plural = 'скидки'
+
+
+class Tax(models.Model):
+    """Модель Tax, которую можно прикрепить к модели Order"""
+    value = models.FloatField(verbose_name='размер налога')
+
+    def __str__(self):
+        return self.value
+
+    class Meta:
+        verbose_name = 'налог'
+        verbose_name_plural = 'налоги'
+
+
+class ItemOrder(models.Model):
+    """Модель ItemOrder, которая связывает Item и Order"""
+    item = models.ForeignKey(Item, verbose_name="товар", on_delete=models.CASCADE)
+    order = models.ForeignKey("Order", verbose_name="заказ", on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(verbose_name="количество")
+
+    def __str__(self):
+        return f"{self.order}: {self.item}, {self.quantity}"
+
+
+class Order(models.Model):
+    """Модель Order, в которой можно объединить несколько Items"""
+    items = models.ManyToManyField(
+        Item, through='ItemOrder', through_fields=('order', 'item')
+    )
+
+    tax = models.ForeignKey(Tax, verbose_name="налог", null=True, blank=True, on_delete=models.SET_NULL)
+    discount = models.ForeignKey(Discount, verbose_name="скидка", null=True, blank=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.items
+
+    class Meta:
+        verbose_name = 'заказ'
+        verbose_name_plural = 'заказы'
